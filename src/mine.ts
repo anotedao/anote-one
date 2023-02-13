f0819bf5a6360e52fcf9a5b23393978f5615106d
Pragmatic <cryptopragmatic@protonmail.com>
2023-02-13 18:50:41 +0100

    mine invite page - mechanics

--- src/mine.ts
+++ src/mine.ts
@@ -2,17 +2,59 @@ import { Signer } from '@waves/signer';
 import { libs } from '@waves/waves-transactions';
 import { ProviderSeed } from '@waves/provider-seed';
 import Cookies from "js-cookie";
+import $ from "jquery";
 
-var savedReferral = Cookies.get('referral');
+var referral = Cookies.get('referral');
+var address = '';
+var seed = '';
 
-function getSeed() {
-    var seed = libs.crypto.randomSeed();
-    console.log(seed);
+function loadData() {
+    var s = localStorage.getItem('seedTemp'); 
+    if (s != null) {
+        seed = s;
+    }
+
+    var a = localStorage.getItem('addressTemp'); 
+    if (a != null) {
+        address = a;
+    }
 }
 
-function initWaves() {
-    // console.log(savedReferral);
-    getSeed();
+async function createUser() {
+    seed = libs.crypto.randomSeed();
+    var signer = new Signer({
+        NODE_URL: 'https://node.anote.digital',
+    });
+    var provider = new ProviderSeed(seed);
+    provider.connect({
+        NODE_URL: 'https://node.anote.digital',
+        NETWORK_BYTE: 55,
+    });
+    signer.setProvider(provider);
+    var user = await signer.login();
+    address = user.address;
+
+    localStorage.setItem("seedTemp", seed);
+    localStorage.setItem("addressTemp", address);
 }
 
-initWaves();
+function userExists() {
+    var ue = false;
+    if (address.length > 0) {
+        ue = true;
+    }
+    return ue;
+}
+
+async function main() {
+    loadData();
+
+    if (!userExists()) {
+        await createUser();
+    }
+
+    console.log(address);
+    $("#seed").html(seed);
+}
+
+main();
